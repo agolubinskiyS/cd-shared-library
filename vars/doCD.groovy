ea56d146559905ff5355e4c4eb6213f6e5c2c12d
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 18:14:20 +0200

    creds

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -6,7 +6,7 @@ def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
             environment {
                 CREDS = credentials('c5295ce1-59e7-4849-b18a-9f64a05e4ef2')
             }
-            echo CREDS
+            sh("echo $CREDS")
             loadScript(name: 'script.sh')
             loadScript(name: 'cct_deploy_utils_fat-2.4.0.sh')
             // utilities.prueba('Mensaje')
