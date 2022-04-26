84fef7594361109049b49792973de1ca25b917d1
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 19:21:27 +0200

    dfdfd

--- src/cosas/Utilities.groovy
+++ src/cosas/Utilities.groovy
@@ -19,16 +19,16 @@ class Utilities implements Serializable {
       version="$params.version" \
       service="$params.service" """)
     }
-    void publishApplication(Map params = [:]) {
-      script.sh(""". ./cct_deploy_utils_fat-2.4.0.sh && \
-      CICDCD_SSO_URL="$params.url" \ 
-      CICDCD_SSO_USER_ID="$params.user" \
-      CICDCD_SSO_USER_PASSWORD="$params.password" \
-      CICDCD_SSO_TENANT="$params.tenant" \
-      publishApplication \
-      --deploymentDescriptor "$params.deploymentDescriptor" \ 
-      --model "$params.model" \
-      --version "$params.version" \
-      --service "$params.service" """)
-    }
+    // void publishApplication(Map params = [:]) {
+    //   script.sh(""". ./cct_deploy_utils_fat-2.4.0.sh && \
+    //   CICDCD_SSO_URL="$params.url" \ 
+    //   CICDCD_SSO_USER_ID="$params.user" \
+    //   CICDCD_SSO_USER_PASSWORD="$params.password" \
+    //   CICDCD_SSO_TENANT="$params.tenant" \
+    //   publishApplication \
+    //   --deploymentDescriptor "$params.deploymentDescriptor" \ 
+    //   --model "$params.model" \
+    //   --version "$params.version" \
+    //   --service "$params.service" """)
+    // }
 }
