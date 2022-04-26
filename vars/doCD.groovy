c567c2e241401da24a6321e6047630b7beab673f
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 13:52:14 +0200

    utilities

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -1,7 +1,7 @@
 def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
     echo "deploying service on: ${baseUrl} with: ${serviceDescriptor} model: ${serviceModel} version: ${serviceVersion} tenant: ${tenantID}"
 
-    def cct = new Utility()
+    def cct = new Utilities()
 
     cct.get()
 }
