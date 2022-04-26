863f4c6702f53037ce68074dc50fce4dfc2d43c6
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 18:18:14 +0200

    scipt

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -3,14 +3,13 @@ def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
 
     node {
         stage("Build Docker Image") {
-            withCredentials([usernamePassword(credentialsId:'c5295ce1-59e7-4849-b18a-9f64a05e4ef2', passwordVariable: 'Password', usernameVariable: 'Username')]) {
-                sh "echo user=${Username} password=${Password}" 
-            }    
             loadScript(name: 'script.sh')
             loadScript(name: 'cct_deploy_utils_fat-2.4.0.sh')
             // utilities.prueba('Mensaje')
             // utilities.post(name: 'Nombre')
-            utilities.publishApplication(name: 'Nombre')
+            withCredentials([usernamePassword(credentialsId:'c5295ce1-59e7-4849-b18a-9f64a05e4ef2', passwordVariable: 'Password', usernameVariable: 'Username')]) {
+                utilities.publishApplication(name: 'Nombre')
+            }    
         }
     }
 }
