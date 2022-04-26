89ed8f282dd9759ca37bdf0ece561f3d87ad8739
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 14:36:51 +0200

    11

--- src/cosas/Utilities.groovy
+++ src/cosas/Utilities.groovy
@@ -2,14 +2,14 @@ package cosas
 
 class Utilities implements Serializable {
 
-    // private final def script
+    private final def script
 
-    // Utilities(def script) {
-    //     this.script = script
-    // }
+    Utilities(def script) {
+        this.script = script
+    }
 
     def get(String repo) {
-        script.sh("echo algo")
+        this.script.sh("echo algo")
         //this.script.sh(script: "echo algo", returnStdout: true).trim()
     }
 }
