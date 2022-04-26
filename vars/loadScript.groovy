aff83e38723385d464ece8915a6191510184804f
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 16:35:16 +0200

    fix load

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,6 +1,6 @@
 
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "scripts/linux/${config.name}"    
+  def scriptcontents = libraryResource "scripts/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 } 
