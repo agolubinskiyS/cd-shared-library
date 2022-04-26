1e4197190b94ffef2e1c821221d7667199117ef5
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 14:59:03 +0200

    sdfdf

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -1,16 +1,16 @@
-// def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
-//     echo "deploying service on: ${baseUrl} with: ${serviceDescriptor} model: ${serviceModel} version: ${serviceVersion} tenant: ${tenantID}"
+def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
+    echo "deploying service on: ${baseUrl} with: ${serviceDescriptor} model: ${serviceModel} version: ${serviceVersion} tenant: ${tenantID}"
 
-//     def cct = new cosas.Utilities()
-//     print(cct.get('myscript'))
+    def cct = new cosas.Utilities()
+    print(cct.get('myscript'))
 
-// }
-
-def call() {
-    node {
-        def cct = new cosas.Utilities()
-        stage("Build Docker Image") {
-            cct.get('myscript')
-        }
-    }
 }
+
+// def call() {
+//     node {
+//         def cct = new cosas.Utilities()
+//         stage("Build Docker Image") {
+//             cct.get('myscript')
+//         }
+//     }
+// }
