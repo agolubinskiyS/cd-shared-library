30823dbbf94deb6891d18f7a18fb01b4ee756e88
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 14:34:41 +0200

    11

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -1,9 +1,8 @@
 def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
     echo "deploying service on: ${baseUrl} with: ${serviceDescriptor} model: ${serviceModel} version: ${serviceVersion} tenant: ${tenantID}"
 
-    script {
         def cct = new cosas.Utilities()
 
         cct.get('myscript')
-    }
+
 }
