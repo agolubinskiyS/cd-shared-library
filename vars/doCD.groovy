b5510466ce7903ec99c216516bc75cc5f9cbd63c
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 18:13:03 +0200

    Adasd

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -3,6 +3,10 @@ def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
 
     node {
         stage("Build Docker Image") {
+            environment {
+                CREDS = credentials('c5295ce1-59e7-4849-b18a-9f64a05e4ef2')
+            }
+            echo CREDS
             loadScript(name: 'script.sh')
             loadScript(name: 'cct_deploy_utils_fat-2.4.0.sh')
             // utilities.prueba('Mensaje')
