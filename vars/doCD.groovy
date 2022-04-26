2cee41b7692249f683849932e8ac093606f6c8f2
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 19:36:19 +0200

    dfdfdf

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -1,6 +1,6 @@
 def call(Map params = [:]){
     def utilities = new cosas.Utilities(this)
-
+    def descriptor = ''
     node {
         stage("Deploy Service on EOS") {
             loadScript(name: 'script.sh')
@@ -8,19 +8,16 @@ def call(Map params = [:]){
             
             def exists = fileExists 'deploymentDescriptor.json'
             if (exists) {
-                echo 'Yes'
-            } else {
-                if (params.deploymentDescriptor != null) {
-                    echo 'HAY VARIABLE'
-                }
-                else {
-                    echo 'NO HAY VARIABLE'    
-                }
+                descriptor = readFile(file:'deploymentDescriptor.json')
+            } else if (params.deploymentDescriptor != null) {
+                descriptor = params.deploymentDescriptor
+            }
+            else {
+                error 'Deployment Descriptor not found'    
             }
 
-
             withCredentials([usernamePassword(credentialsId:'cct-api', passwordVariable: 'Password', usernameVariable: 'Username')]) {
-                utilities.deploy(url: params.url, user: Username, password: Password, tenant: params.tenant, deploymentDescriptor: params.descriptor, model: params.model, version: params.version, service: params.service)
+                utilities.deploy(url: params.url, user: Username, password: Password, tenant: params.tenant, deploymentDescriptor: descriptor, model: params.model, version: params.version, service: params.service)
                 //utilities.publishApplication(url: params.url, user: Username, password: Password, tenant: params.tenant, deploymentDescriptor: params.descriptor, model: params.model, version: params.version, service: params.service)
             }    
         }
