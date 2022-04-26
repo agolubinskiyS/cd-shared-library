ea14fb6621747b1ea0c633251d9eae99d037229f
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 18:46:37 +0200

    dfdf

--- src/cosas/Utilities.groovy
+++ src/cosas/Utilities.groovy
@@ -18,26 +18,26 @@ class Utilities implements Serializable {
       // script.sh("echo ${params.name}")
       // script.echo params.name
       script.sh("""echo \
-      CICDCD_SSO_URL=${params.url} \ 
-      CICDCD_SSO_USER_ID=${params.user} \
-      CICDCD_SSO_USER_PASSWORD=${params.password} \
-      CICDCD_SSO_TENANT=${params.password} \
-      publishApplication \
-      --deploymentDescriptor ${params.deploymentDescriptor} \ 
-      --model ${params.model} \
-      --version ${params.version} \
-      --service ${params.service}""")
+      CICDCD_SSO_URL=$params.url
+      CICDCD_SSO_USER_ID=$params.user
+      CICDCD_SSO_USER_PASSWORD=$params.password
+      CICDCD_SSO_TENANT=$params.password
+      publishApplication
+      deploymentDescriptor $params.deploymentDescriptor
+      model $params.model
+      version $params.version
+      service $params.service""")
     }
-    void publishApplication(Map params = [:]) {
-      script.sh(""". ./cct_deploy_utils_fat-2.4.0.sh && \
-      CICDCD_SSO_URL="${params.url}" \ 
-      CICDCD_SSO_USER_ID="${params.user}" \
-      CICDCD_SSO_USER_PASSWORD="${params.password}" \
-      CICDCD_SSO_TENANT="${params.password}" \
-      publishApplication \
-      --deploymentDescriptor "${params.deploymentDescriptor}" \ 
-      --model "${params.model}" \
-      --version "${params.version}" \
-      --service "${params.service}" """)
-    }
+    // void publishApplication(Map params = [:]) {
+    //   script.sh(""". ./cct_deploy_utils_fat-2.4.0.sh && \
+    //   CICDCD_SSO_URL="${params.url}" \ 
+    //   CICDCD_SSO_USER_ID="${params.user}" \
+    //   CICDCD_SSO_USER_PASSWORD="${params.password}" \
+    //   CICDCD_SSO_TENANT="${params.password}" \
+    //   publishApplication \
+    //   --deploymentDescriptor "${params.deploymentDescriptor}" \ 
+    //   --model "${params.model}" \
+    //   --version "${params.version}" \
+    //   --service "${params.service}" """)
+    // }
 }
