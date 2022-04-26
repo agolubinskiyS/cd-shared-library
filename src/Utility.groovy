ee7f9521a0b88fc32d664edca7274382e568b888
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 13:35:54 +0200

    Add class structure

---
+++ src/Utility.groovy
@@ -0,0 +1,13 @@
+class CCT {
+    def url = "https://httpbin.org/get"
+
+    def get(url) {
+        sh """
+        curl -k -X GET --url $url \
+        -H 'accept: */*'
+        """
+    }   
+
+}
+
+
--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -1,3 +1,7 @@
 def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
     echo "deploying service on: ${baseUrl} with: ${serviceDescriptor} model: ${serviceModel} version: ${serviceVersion} tenant: ${tenantID}"
+
+    def cct = new CCT()
+
+    cct.get()
 }
