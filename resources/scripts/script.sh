36df16b7a0644be7f24b343fac55f731fa6b6759
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 19:19:55 +0200

    fgfgfg

--- resources/scripts/script.sh
+++ resources/scripts/script.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
+for PARAMETER in "$@"
+do
+   KEY=$(echo $PARAMETER | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${PARAMETER:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+echo "CICDCD_SSO_URL = $CICDCD_SSO_URL"
+echo "CICDCD_SSO_TENANT = $CICDCD_SSO_TENANT"
+echo "deploymentDescriptor = $deploymentDescriptor"
+echo "model = $model"
+echo "version = $version"
+echo "service = $service"
+
 # curl -k -X POST https://bootstrap.yankee.labs.stratio.com/service/cct-deploy-api/deploy/grafana/adminrouter/11.0.1/schema?tenantId=s000004 \
 # -H 'Cookie: dcos-acs-auth-cookie=eyJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldCIsInR5cCI6IkpXVCJ9.eyJjbiI6ImFkbWluIiwiZXhwIjoxNjUwOTA3NzA2LCJncm91cHMiOlsiYWRtaW4iLCJzdHJhdGlvIiwibWFuYWdlcl9pbnN0YWxsZXIiLCJtYW5hZ2VyX2FkbWluIiwiczAwMDAwMi1TdXBlckFkbWluLWdvdmVybmFuY2UiLCJzMDAwMDAyLWdvdmVybmFuY2Utc3VwZXItYWRtaW5zIiwiczAwMDAwMy1TdXBlckFkbWluLWdvdmVybmFuY2UiLCJzMDAwMDAzLWdvdmVybmFuY2Utc3VwZXItYWRtaW5zIiwibWRtX2FkbWluIiwiczAwMDAwMy1kaXNjb3ZlcnktYWRtaW4iXSwibWFpbCI6ImFkbWluQHlhbmtlZS5sYWJzLnN0cmF0aW8uY29tIiwidGVuYW50IjoiTk9ORSIsInVpZCI6ImFkbWluIn0.IosEt1H7-68zIh8gIXInoE7JruELNx8gqDGAcnbTrlY' \
 # -H 'Content-Type: application/json' \
 # -H 'accept: */*' \
 # -d '{"general":{"serviceId":"s000004/s000004-grafana-prueba","appname":"grafana-prueba","grafanaAdminUser":"admin","identity":{"approlename":"s000004"},"network":{"networkName":"s000004-core"},"resources":{"INSTANCES":1,"CPUs":1,"MEM":1024}},"settings":{"logs":{"nginxLogLevel":"error"}},"placement":{"marathonConstraintSection":{"marathonConstraintName":"","marathonConstraintOperator":"","marathonConstraintValue":""}},"environment":{"grafanaConsulDomain":"${eos.internalDomain}","grafanaSSOURI":"${globals.sso.ssoUri}","vault":{"vaultHosts":"vault.service.eos.yankee.labs.stratio.com","vaultPort":8200}}}'
-
-
-echo "USER IS ${1} PASSWORD is ${2}"
--- src/cosas/Utilities.groovy
+++ src/cosas/Utilities.groovy
@@ -10,14 +10,14 @@ class Utilities implements Serializable {
 
     void deploy(Map params = [:]) {
       script.sh("""./script.sh \
-      CICDCD_SSO_URL=$params.url \
-      CICDCD_SSO_USER_ID=$params.user \
-      CICDCD_SSO_USER_PASSWORD=$params.password \
-      CICDCD_SSO_TENANT=$params.tenant \
-      deploymentDescriptor $params.deploymentDescriptor \
-      model $params.model \
-      version $params.version \
-      service $params.service""")
+      CICDCD_SSO_URL="$params.url" \
+      CICDCD_SSO_USER_ID="$params.user" \
+      CICDCD_SSO_USER_PASSWORD="$params.password" \
+      CICDCD_SSO_TENANT="$params.tenant" \
+      deploymentDescriptor="$params.deploymentDescriptor" \
+      model="$params.model" \
+      version="$params.version" \
+      service="$params.service" """)
     }
     void publishApplication(Map params = [:]) {
       script.sh(""". ./cct_deploy_utils_fat-2.4.0.sh && \
