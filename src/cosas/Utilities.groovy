8649468d41dd2db1daba7adaabb383a03f227348
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 16:08:49 +0200

    ewrwer

--- src/cosas/Utilities.groovy
+++ src/cosas/Utilities.groovy
@@ -11,8 +11,8 @@ class Utilities implements Serializable {
     }
 
     void get(String msg) {
-      script.echo msg
-      //script.sh("echo algo")
+      //script.echo msg
+      sh("echo algo")
       // this.log.debug ("isReleaseActio")
       //this.script.sh("echo algo")
       //this.script.sh(script: "echo algo", returnStdout: true).trim()
