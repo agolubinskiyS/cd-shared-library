1eae6c10b605a93d3b6e168aee17847d5884bc37
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 18:16:40 +0200

    creds

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -3,10 +3,9 @@ def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
 
     node {
         stage("Build Docker Image") {
-            environment {
-                CREDS = credentials('c5295ce1-59e7-4849-b18a-9f64a05e4ef2')
-            }
-            sh("echo $CREDS")
+            withCredentials([usernamePassword(credentialsId:'c5295ce1-59e7-4849-b18a-9f64a05e4ef2', passwordVariable: 'Password', usernameVariable: 'Username')]) {
+                sh "echo user=${Username} password=${Password}" 
+            }    
             loadScript(name: 'script.sh')
             loadScript(name: 'cct_deploy_utils_fat-2.4.0.sh')
             // utilities.prueba('Mensaje')
