c48cfddefe1e95ae22236406c99110749b8cac68
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 16:21:13 +0200

    ls

--- src/cosas/Utilities.groovy
+++ src/cosas/Utilities.groovy
@@ -13,6 +13,7 @@ class Utilities implements Serializable {
     void get(String msg) {
       script.echo msg
       script.sh("pwd")
+      script.sh("ls")
       script.sh("./resources/script.sh")
     }
 }
