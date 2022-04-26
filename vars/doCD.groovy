786ff34160e1a8750c8cbca4332d8e7e1268b222
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-26 17:49:42 +0200

    cscsc

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -5,8 +5,8 @@ def call(baseUrl, serviceDescriptor, serviceModel, serviceVersion, tenantID){
         stage("Build Docker Image") {
             loadScript(name: 'script.sh')
             loadScript(name: 'cct_deploy_utils_fat-2.4.0.sh')
-            utilities.prueba('Mensaje')
-            utilities.post(name: 'Nombre')
+            // utilities.prueba('Mensaje')
+            // utilities.post(name: 'Nombre')
             utilities.publishApplication(name: 'Nombre')
         }
     }
