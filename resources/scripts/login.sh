45818073fc1c8afc413950eef50cdf13d4d96fb2
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-27 14:04:52 +0200

    fgfgfg

--- resources/scripts/login.sh
+++ resources/scripts/login.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-. ./login_mock.sh && CICDCD_SSO_URL="$1" CICDCD_SSO_USER_ID="$2" CICDCD_SSO_USER_PASSWORD="$3" CICDCD_SSO_TENANT="$4" login_CCT --rawcookies
+. ./sso_login-2.4.0.sh && CICDCD_SSO_URL="$1" CICDCD_SSO_USER_ID="$2" CICDCD_SSO_USER_PASSWORD="$3" CICDCD_SSO_TENANT="$4" login_CCT --rawcookies
