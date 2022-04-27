6e3493e9880af180ee6be6b4d4afa9f360044efd
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-27 16:52:16 +0200

    sdfdf

--- src/tools/Utilities.groovy
+++ src/tools/Utilities.groovy
@@ -30,8 +30,6 @@ class Utilities implements Serializable {
       service="$params.service"
       cookie="$cookie"         
       """
-      // command = command.replaceAll("(\\t|\\r?\\n)+", " ")
-      script.echo(command)
       script.sh(returnStdout: true, script: command + " publishApplication")
     }
 }
