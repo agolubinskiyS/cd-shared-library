2265dd97c828c18769573f39fd9f35c382ec27b7
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-27 13:41:53 +0200

    dfdfd

--- src/tools/Utilities.groovy
+++ src/tools/Utilities.groovy
@@ -15,7 +15,7 @@ class Utilities implements Serializable {
     public login(Map params = [:]) {
       def operation = String.format("./login.sh %s %s %s %s", params.url, params.user, params.password, params.tenant)
       def cookie = script.sh(returnStdout: true, script: operation).trim().substring(21)
-      script.echo cookie.substring(0, cookie.indexOf('='))
+      script.echo cookie.substring(0, cookie.indexOf(';'))
     }
     void publishApplication(Map params = [:]) {
       script.sh("""./script.sh \
