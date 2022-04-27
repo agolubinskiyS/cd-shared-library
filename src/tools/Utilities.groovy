0270a8c79ba17c3a4a5b7993f4ec5fec591ae843
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-27 13:43:34 +0200

    dfdfdf

--- src/tools/Utilities.groovy
+++ src/tools/Utilities.groovy
@@ -15,17 +15,18 @@ class Utilities implements Serializable {
     public login(Map params = [:]) {
       def operation = String.format("./login.sh %s %s %s %s", params.url, params.user, params.password, params.tenant)
       def cookie = script.sh(returnStdout: true, script: operation).trim().substring(21)
-      script.echo cookie.substring(0, cookie.indexOf(';'))
+      return cookie.substring(0, cookie.indexOf(';'))
     }
     void publishApplication(Map params = [:]) {
-      script.sh("""./script.sh \
-      CICDCD_SSO_URL="$params.url" \
-      CICDCD_SSO_USER_ID="$params.user" \
-      CICDCD_SSO_USER_PASSWORD="$params.password" \
-      CICDCD_SSO_TENANT="$params.tenant" \
-      deploymentDescriptor="$params.deploymentDescriptor" \
-      model="$params.model" \
-      version="$params.version" \
-      service="$params.service" """)
+      script.echo this.cookie
+      // script.sh("""./script.sh \
+      // CICDCD_SSO_URL="$params.url" \
+      // CICDCD_SSO_USER_ID="$params.user" \
+      // CICDCD_SSO_USER_PASSWORD="$params.password" \
+      // CICDCD_SSO_TENANT="$params.tenant" \
+      // deploymentDescriptor="$params.deploymentDescriptor" \
+      // model="$params.model" \
+      // version="$params.version" \
+      // service="$params.service" """)
     }
 }
