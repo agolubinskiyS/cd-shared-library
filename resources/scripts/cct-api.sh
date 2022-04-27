5ae6ece2fdb17d757936c701ccbd30baf93267e9
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-27 18:39:39 +0200

    dfdfd

--- resources/scripts/cct-api.sh
+++ resources/scripts/cct-api.sh
@@ -18,20 +18,19 @@ publishApplication() {
    echo "version=$version"
    echo "service=$service"
    echo "cookie=$cookie"
-   # status_code=$(curl --write-out %{http_code} --silent --output /dev/null -k \
-   # -X POST $CICDCD_SSO_URL/service/cct-deploy-api/deploy/$service/$model/$version/schema?tenantId=$CICDCD_SSO_TENANT \
-   # -H "Cookie: dcos-acs-auth-cookie=$cookie" \
-   # -H 'Content-Type: application/json' \
-   # -H 'accept: */*' \
-   # -d "$deploymentDescriptor")
+   status_code=$(curl --write-out %{http_code} --silent --output /dev/null -k \
+   -X POST $CICDCD_SSO_URL/service/cct-deploy-api/deploy/$service/$model/$version/schema?tenantId=$CICDCD_SSO_TENANT \
+   -H "Cookie: dcos-acs-auth-cookie=$cookie" \
+   -H 'Content-Type: application/json' \
+   -H 'accept: */*' \
+   -d "$deploymentDescriptor")
 
-   # if [[ "$status_code" -ne 202 ]] ; then
-   # echo "Error: Deployment status code: $status_code"
-   # else
-   # echo "OK! Deployment started"
-   # exit 0
-   # fi
+   if [[ "$status_code" -ne 202 ]] ; then
+   echo "Error: Deployment status code: $status_code"
+   else
+   echo "OK! Deployment started"
    exit 0
+   fi
 }
 
 upgradeApplication() {
