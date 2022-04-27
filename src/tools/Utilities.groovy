9ec9ee6e8f5e29f5ba1038157f698fc755c008fb
agolubinskiyS <agolubinskiy@stratio.com>
2022-04-27 18:31:30 +0200

    dfdf

--- src/tools/Utilities.groovy
+++ src/tools/Utilities.groovy
@@ -27,7 +27,6 @@ class Utilities implements Serializable {
       def operation = String.format("./login.sh %s %s %s %s", params.url, Username, Password, params.tenant)
       def result = script.sh(returnStdout: true, script: operation).trim().substring(21)
       cookie = result.substring(0, result.indexOf(';')) 
-      cookie = null
       if (isNullOrEmpty(cookie)) { throw new RuntimeException("Login failed") }
       command = command + """cookie="$cookie" """
     }
