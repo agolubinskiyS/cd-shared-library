144e0863d72a236982cb409c1afd0377ffa87260
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-02 11:57:03 +0200

    Pod template

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -2,28 +2,29 @@ def call(Map params = [:]){
     def utilities = new tools.Utilities(this, params)
     def scripts = ['cct-api.sh', 'sso_login-2.4.0.sh', 'login_mock.sh', 'login.sh']
     def descriptor = ''
+    podTemplate(containers: [containerTemplate(name: "curl", image: "dwdraju/alpine-curl-jq", command: "sleep", args: "9999999")]) {
+        node {
+            stage("Deploy Service on EOS") {
+            
+                loadScripts(scripts)
 
-    node {
-        stage("Deploy Service on EOS") {
-        
-            loadScripts(scripts)
+                def exists = fileExists 'deploymentDescriptor.json'
+                if (exists) {
+                    descriptor = readFile(file:'deploymentDescriptor.json')
+                } else if (params.deploymentDescriptor != null) {
+                    descriptor = params.deploymentDescriptor
+                }
+                else {
+                    error 'Deployment Descriptor not found'    
+                }
 
-            def exists = fileExists 'deploymentDescriptor.json'
-            if (exists) {
-                descriptor = readFile(file:'deploymentDescriptor.json')
-            } else if (params.deploymentDescriptor != null) {
-                descriptor = params.deploymentDescriptor
+                withCredentials([usernamePassword(credentialsId:'cct-api', passwordVariable: 'Password', usernameVariable: 'Username')]) {
+                    utilities.login(Username, Password)
+                }
+                def escaped_json = groovy.json.JsonOutput.toJson(params.descriptor)
+                utilities.publishApplication(deploymentDescriptor: escaped_json, model: params.model, version: params.version, service: params.service)    
+            
             }
-            else {
-                error 'Deployment Descriptor not found'    
-            }
-
-            withCredentials([usernamePassword(credentialsId:'cct-api', passwordVariable: 'Password', usernameVariable: 'Username')]) {
-                utilities.login(Username, Password)
-            }
-            def escaped_json = groovy.json.JsonOutput.toJson(params.descriptor)
-            utilities.publishApplication(deploymentDescriptor: escaped_json, model: params.model, version: params.version, service: params.service)    
-        
         }
     }
 }
