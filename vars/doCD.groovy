b1a2056fcdd3211d3ff845202c94aa95f6de6788
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-02 14:52:24 +0200

    tiemout

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -6,28 +6,28 @@ def call(Map params = [:]){
         node(POD_LABEL) {
             // dir('build') {
                 stage("Deploy Service on EOS") {
-                
-                    loadScripts(scripts)
-                    sh("pwd")
-                    sh("ls -lha")
-                    def exists = fileExists 'deploymentDescriptor.json'
-                    if (exists) {
-                        descriptor = readFile(file:'deploymentDescriptor.json')
-                    } else if (params.deploymentDescriptor != null) {
-                        descriptor = params.deploymentDescriptor
-                    }
-                    else {
-                        error 'Deployment Descriptor not found'    
-                    }
+                    timeout(time: 10, unit: 'MINUTES') {
+                        loadScripts(scripts)
+                        sh("pwd")
+                        sh("ls -lha")
+                        def exists = fileExists 'deploymentDescriptor.json'
+                        if (exists) {
+                            descriptor = readFile(file:'deploymentDescriptor.json')
+                        } else if (params.deploymentDescriptor != null) {
+                            descriptor = params.deploymentDescriptor
+                        }
+                        else {
+                            error 'Deployment Descriptor not found'    
+                        }
 
-                    assert params.url ==~ $/http(s)?://.+?/$ : 'unexpected CCT url format'
+                        assert params.url ==~ $/http(s)?://.+?/$ : 'unexpected CCT url format'
 
-                    withCredentials([usernamePassword(credentialsId:'cct-api', passwordVariable: 'Password', usernameVariable: 'Username')]) {
-                        utilities.login(Username, Password)
+                        withCredentials([usernamePassword(credentialsId:'cct-api', passwordVariable: 'Password', usernameVariable: 'Username')]) {
+                            utilities.login(Username, Password)
+                        }
+                        def escaped_json = groovy.json.JsonOutput.toJson(params.descriptor)
+                        utilities.publishApplication(deploymentDescriptor: escaped_json, model: params.model, version: params.version, service: params.service)    
                     }
-                    def escaped_json = groovy.json.JsonOutput.toJson(params.descriptor)
-                    utilities.publishApplication(deploymentDescriptor: escaped_json, model: params.model, version: params.version, service: params.service)    
-
                 }
         //    }
         }
