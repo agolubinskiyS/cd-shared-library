83dd3211ba35f391e2b1616970179b714eab93db
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-02 16:55:55 +0200

    dfdfdf

--- resources/scripts/cct-api.sh
+++ resources/scripts/cct-api.sh
@@ -10,28 +10,24 @@ do
    export "$KEY"="$VALUE"
 done
 
-publishApplication() {
-   echo "CICDCD_SSO_URL=$CICDCD_SSO_URL"
-   echo "CICDCD_SSO_TENANT=$CICDCD_SSO_TENANT"
-   echo """$deploymentDescriptor"""
-   echo "model=$model"
-   echo "version=$version"
-   echo "service=$service"
-   echo "cookie=$cookie"
-   status_code=$(curl --write-out %{http_code} --silent --output /dev/null -k \
-   -X POST $CICDCD_SSO_URL/service/cct-deploy-api/deploy/$service/$model/$version/schema?tenantId=$CICDCD_SSO_TENANT \
-   -H "Cookie: dcos-acs-auth-cookie=$cookie" \
-   -H 'Content-Type: application/json' \
-   -H 'accept: */*' \
-   -d """$deploymentDescriptor""")
-
-   if [[ "$status_code" -ne 202 ]] ; then
+statusValidate(){  
+   if [[ "$1" -ne 202 ]] ; then
    echo "Error: Deployment status code: $status_code"
    exit 2
    else
    echo "OK! Deployment started"
    exit 0
    fi
+}
+
+publishApplication() {
+   status_code=$(curl --write-out %{http_code} --silent --output /dev/null -k \
+   -X POST $CICDCD_SSO_URL/service/cct-deploy-api/deploy/$service/$model/$version/schema?tenantId=$CICDCD_SSO_TENANT \
+   -H "Cookie: dcos-acs-auth-cookie=$cookie" \
+   -H 'Content-Type: application/json' \
+   -H 'accept: */*' \
+   -d """$deploymentDescriptor""")
+   statusValidate status_code
 }
 
 upgradeApplication() {
