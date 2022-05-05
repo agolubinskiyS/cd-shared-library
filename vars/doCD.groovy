8a75730727e77232a28efe30f217c16f79e33a6d
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-05 16:32:03 +0200

    dfdf

--- vars/doCD.groovy
+++ vars/doCD.groovy
@@ -27,7 +27,7 @@ def call(Map params = [:]){
                         descriptor = groovy.json.JsonOutput.toJson(descriptor.replace("\n", "").replace(" ", "").trim())
                         def toJsonConverter = new tools.ToJsonConverter(descriptor)
 
-                        println(toJsonConverter.parseJsonText())
+                        println(toJsonConverter.parseJsonText().general)
 
                         assert params.url ==~ $/http(s)?://.+?/$ : 'unexpected CCT url format'
 
