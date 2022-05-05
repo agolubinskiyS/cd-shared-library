6454d0e090fec48ed4edffdcd3d9cbe80143ce91
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-05 17:50:58 +0200

    dfdf

--- vars/assertParams.groovy
+++ vars/assertParams.groovy
@@ -1,5 +1,5 @@
 def call(params) {
     assert params.url ==~ $/http(s)?://.+?/$ : 'unexpected CCT url format'
     assert params.version ==~ $/(\d+)\.(\d+)\.(\d+)/$ : 'unexpected version format'
-    assert params.model ==~ $/[A-z]/$ : 'unexpected model format'
+    assert params.model ==~ $/[a-zA-Z]/$ : 'unexpected model format'
 } 
