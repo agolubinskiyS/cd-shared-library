20e5195aec5829f46353082ccbeb7f7fd37b09ea
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-07 12:31:05 +0200

    fgfgfg

--- src/tools/Utilities.groovy
+++ src/tools/Utilities.groovy
@@ -52,7 +52,7 @@ class Utilities implements Serializable {
 
     void updateServiceDescriptor(String serviceDescriptor) {
       if (isNullOrEmpty(serviceDescriptor)) { throw new RuntimeException("serviceDescriptor error") }
-      script.sh("""#!/bin/bash  echo $serviceDescriptor""")
+      // script.sh("""#!/bin/bash  echo $serviceDescriptor""")
       command = command + """serviceDescriptor=$serviceDescriptor """
       script.sh(returnStdout: true, script: command + " updateServiceDescriptor")
     }
