943ca8ae4226eef58219cf4b4b27ec5598e02df6
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-07 18:01:10 +0200

    dfdfdf

--- resources/scripts/cct-api.sh
+++ resources/scripts/cct-api.sh
@@ -77,7 +77,7 @@ prueba() {
 
 parametrizeImage() {
    tmp=$(mktemp)
-   echo $image > /tmp/output
+   echo '${eos.dockerRegistry}/'$image > /tmp/output
    # jq --arg i "$image" '.data.container.runners[0].image = $i' \
    # saas-universe/maintenance-core-default.json > "$tmp" && mv "$tmp" saas-universe/maintenance-core-default.json
 }
