cbcae9258577b17b939d0691dd43bb20638ebf09
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-09 12:53:17 +0200

    dfdfdf

--- src/tools/Utilities.groovy
+++ src/tools/Utilities.groovy
@@ -72,7 +72,7 @@ class Utilities implements Serializable {
         descriptor = params.deploymentDescriptor
       }
       else if (file.exists()) {
-        descriptor = file.readFile(file: deploymentDescriptorPath)
+        descriptor = file.readFileString(deploymentDescriptorPath)
       } else { 
         throw new RuntimeException("Deployment Descriptor not found") 
       }
