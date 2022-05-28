2bff2f910248e9b3e53983dc8bf6864610d784e1
agolubinskiyS <agolubinskiy@stratio.com>
2022-05-28 18:10:41 +0200

    fgfg

--- src/tools/RecursiveJson.groovy
+++ src/tools/RecursiveJson.groovy
@@ -14,7 +14,7 @@ class RecursiveJson {
 
     RecursiveJson() {
     }
-    
+    @NonCPS
     void toMap(Object json, String key, Map result){
         json = [(key): json]
         json.each {k, v ->
@@ -33,7 +33,7 @@ class RecursiveJson {
         }
         }
     }
-    
+    @NonCPS
     void filteredValues(String crude, Boolean value, List filtered) {
         String pattern = /.*\.(.*?)\.ui.visible\.(.*)/   
         def m = crude =~ pattern
@@ -41,7 +41,7 @@ class RecursiveJson {
         filtered.add(f)
     }
 
-    
+    @NonCPS
     void pruneDescriptorHelper(String a, String b, Boolean c, Map descriptorJson) {
         //NEED TO FIND b in fields and verify that b + c is the same in the descriptorJson othewise delete a
         if (descriptorJson instanceof Map) {
@@ -76,7 +76,7 @@ class RecursiveJson {
         }
     }
     
-    
+    @NonCPS
     void pruneDescriptor(List filtered, Object descriptorJson){
         filtered.each { a, b, c ->
             pruneDescriptorHelper(a, b, c, descriptorJson)
